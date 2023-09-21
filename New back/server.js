6cf4d7ea053a3cde04fcc77319263799c0874cb7
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-21 10:53:31 +0000

    Update server.js

--- New back/server.js
+++ New back/server.js
@@ -27,19 +27,22 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
-// app.use(cors(
-//   {
-//     origin:["https:deploy .vercel.app"],
-//     methods:["POST", "GET","DELETE","PUT"],
-//     credentials:true
-//   }
-// ))
+//deploy
+// app.use(
+//   cors({
+//     origin: true,
+//     credentials: true,
+//   })
+// );
+
+//deploy
+app.use(cors(
+  {
+    origin:["newrecords.vercel.app"],
+    methods:["POST", "GET","DELETE","PUT"],
+    credentials:true
+  }
+))
 //DB connection
 connectToDb();
 
