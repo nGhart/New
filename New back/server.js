d6122949a2fc9b0e9e5aabc7b8597be841b3e98a
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-21 11:40:50 +0000

    Update server.js

--- New back/server.js
+++ New back/server.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 //deploy
 app.use(cors(
   {
-    origin:[""],
+    origin:["https://newfrontside.vercel.app/"],
     methods:["POST", "GET","DELETE","PUT"],
     credentials:true
   }
