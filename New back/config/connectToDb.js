1108dfa7c950c42d66525950855c26fb54482675
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-21 10:42:48 +0000

    Update connectToDb.js deploy

--- New back/config/connectToDb.js
+++ New back/config/connectToDb.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 async function connectToDb() {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    //deploy changes
+    await mongoose.connect(mongodb+srv://root:root@demo.uw7o4th.mongodb.net/demo?retryWrites=true&w=majority);
     console.log('Connected to DB');
   } catch (error) {
     console.log(error.message);
